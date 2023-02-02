a04ea00920755db327b1025ad187f244cf1b7767
Claudio Chimera <Claudio.Chimera@gmail.com>
2023-02-02 14:24:16 +0100

    NOT_VERIFY_SSL

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,6 @@
 /**
  * NodeRED Alexa SmartHome
- * Copyright 2022 Claudio Chimera <Claudio.Chimera at gmail.com>.
+ * Copyright 2023 Claudio Chimera <Claudio.Chimera at gmail.com>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,72 +17,76 @@
  **/
 
 
- const https = require('https')
- const base_url = process.env.BASE_URL
- const d = 'True' == process.env.DEBUG
- 
- const { hostname = "localhost", pathname = "/", s_port, protocol } = new URL(base_url);
- const port = s_port ? parseInt(s_port) : protocol === 'https:' ? 443 : 80
- 
- if (d) {
-   console.log('url: ' + base_url)
-   console.log('hostname: ' + hostname)
-   console.log('pathname: ' + pathname)
-   console.log('port: ' + port)
-   console.log('protocol: ' + protocol)
- }
- 
- const options = {
-   hostname: hostname,
-   path: pathname,
-   method: 'POST',
-   port: port,
-   headers: {
-     'Content-Type': 'application/json',
-   },
- };
- 
- function getError(err) {
-   return {
-     'event': {
-       'payload': {
-         'type': 'INTERNAL_ERROR',
-         'message': err,
-       }
-     }
-   };
- }
- 
- exports.handler = async (event) => {
-   if (d) {
-     console.log("event: " + JSON.stringify(event))
-   }
- 
-   return new Promise((resolve, reject) => {
-     const req = https.request(options, res => {
-       let rawData = '';
- 
-       res.on('data', chunk => {
-         rawData += chunk;
-       });
- 
-       res.on('end', () => {
-         try {
-           resolve(JSON.parse(rawData));
-         } catch (err) {
-           if (d) console.log("rawData: " + rawData)
-           resolve(getError(rawData));
-         }
-       });
-     });
- 
-     req.on('error', err => {
-       if (d) console.log("reject: " + err)
-       resolve(getError(err));
-     });
- 
-     req.write(JSON.stringify(event));
-     req.end();
-   });
- };
- 
+const https = require('https')
+const base_url = process.env.BASE_URL
+const not_verify_ssl = 'True' == process.env.NOT_VERIFY_SSL 
+const d = 'True' == process.env.DEBUG
+
+const { hostname = "localhost", pathname = "/", port, protocol } = new URL(base_url);
+const s_port = port ? parseInt(port) : protocol === 'https:' ? 443 : 80
+
+if (d) {
+  console.log('url: ' + base_url)
+  console.log('hostname: ' + hostname)
+  console.log('pathname: ' + pathname)
+  console.log('port: ' + s_port)
+  console.log('protocol: ' + protocol)
+}
+
+const options = {
+  hostname: hostname,
+  path: pathname,
+  method: 'POST',
+  port: s_port,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+function getError(err) {
+  return {
+    'event': {
+      'payload': {
+        'type': 'INTERNAL_ERROR',
+        'message': err,
+      }
+    }
+  };
+}
+
+exports.handler = async (event) => {
+  if (d) {
+    console.log("event: " + JSON.stringify(event))
+  }
+
+  if (not_verify_ssl) {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+      let rawData = '';
+
+      res.on('data', chunk => {
+        rawData += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(rawData));
+        } catch (err) {
+          if (d) console.log("rawData: " + rawData)
+          resolve(getError(rawData));
+        }
+      });
+    });
+
+    req.on('error', err => {
+      if (d) console.log("reject: " + err)
+      resolve(getError(err));
+    });
+
+    req.write(JSON.stringify(event));
+    req.end();
+  });
+};
